c0711c646daed8bd5cd6698054a8422695752477
MakiseKurisu <saberconer@gmail.com>
2019-01-15 23:28:20 -0600

    Add SizeVerb

---
+++ Everything.NET/Verbs/SizeVerb.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using Everything.NET.Library;
+using System;
+using System.Collections.Generic;
+
+namespace Everything.NET.Verbs
+{
+    [Verb("size", HelpText = "Return size of a given target.")]
+    public class SizeVerb: SearchParameter, IVerbBase
+    {
+        [Usage]
+        public static IEnumerable<Example> Examples
+        {
+            get => new List<Example>() {
+                new Example("Return size of a given target.", new SizeVerb { _Uri = @"http://www.example.com:8080/C:"})
+            };
+        }
+
+        public int Action()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
