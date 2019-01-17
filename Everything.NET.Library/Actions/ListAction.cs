672cd8cddc16d75fe95624e4bdaeac2e3175c384
MakiseKurisu <saberconer@gmail.com>
2019-01-16 20:56:38 -0600

    Implement Size for FolderResource

--- Everything.NET.Library/Actions/ListAction.cs
+++ Everything.NET.Library/Actions/ListAction.cs
@@ -1,5 +1,6 @@
 ﻿using Everything.NET.Library.Types.Queries;
 using Everything.NET.Library.Types.Resources;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,10 +10,21 @@ namespace Everything.NET.Library.Actions
     {
         public static List<BaseResource> Action(string uri, BaseQuery query)
         {
-            return ActionAsync(uri, query).Result;
+            return ActionAsync(new Uri(uri), query).Result;
         }
 
         public static async Task<List<BaseResource>> ActionAsync(string uri, BaseQuery query)
+        {
+            var ret = await ActionAsync(new Uri(uri), query);
+            return ret;
+        }
+
+        public static List<BaseResource> Action(Uri uri, BaseQuery query)
+        {
+            return ActionAsync(uri, query).Result;
+        }
+
+        public static async Task<List<BaseResource>> ActionAsync(Uri uri, BaseQuery query)
         {
             using (var req = new Request(uri))
             {
--- Everything.NET.Library/Types/Resources/FolderResource.cs
+++ Everything.NET.Library/Types/Resources/FolderResource.cs
@@ -1,10 +1,24 @@
-﻿using Everything.NET.Library.RawTypes;
+﻿using Everything.NET.Library.Actions;
+using Everything.NET.Library.RawTypes;
 
 namespace Everything.NET.Library.Types.Resources
 {
     public class FolderResource: BaseResource
     {
-        public override FileSize Size { get; set; }
+        public override FileSize Size
+        {
+            get
+            {
+                var contents = ListAction.Action(Uri, new Queries.BaseQuery());
+                var size = new FileSize();
+                foreach (var c in contents)
+                {
+                    size += c.Size;
+                }
+                return size;
+            }
+            set { }
+        }
 
         public FolderResource(RawResource obj) : base(obj) { }
     }
