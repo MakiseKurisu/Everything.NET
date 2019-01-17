d9daab8939781edc47fd7e9510910c8e2bc35df2
MakiseKurisu <saberconer@gmail.com>
2019-01-16 21:09:47 -0600

    Stub SizeAction

---
+++ Everything.NET.Library/Actions/SizeAction.cs
@@ -0,0 +1,17 @@
+﻿using Everything.NET.Library.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everything.NET.Library.Actions
+{
+    public class SizeAction
+    {
+        public async static Task<FileSize> Action(string uri)
+        {
+            return null;
+        }
+    }
+}
