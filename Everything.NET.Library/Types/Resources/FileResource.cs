9adab4a8530a37aa32e31d00236dc057f14e6b08
MakiseKurisu <saberconer@gmail.com>
2019-01-16 20:32:48 -0600

    Add 2 more resources

---
+++ Everything.NET.Library/Types/Resources/FileResource.cs
@@ -0,0 +1,9 @@
+﻿using Everything.NET.Library.RawTypes;
+
+namespace Everything.NET.Library.Types.Resources
+{
+    public class FileResource: BaseResource
+    {
+        public FileResource(RawResource obj) : base(obj) { }
+    }
+}
---
+++ Everything.NET.Library/Types/Resources/FolderResource.cs
@@ -0,0 +1,9 @@
+﻿using Everything.NET.Library.RawTypes;
+
+namespace Everything.NET.Library.Types.Resources
+{
+    public class FolderResource: BaseResource
+    {
+        public FolderResource(RawResource obj) : base(obj) { }
+    }
+}
