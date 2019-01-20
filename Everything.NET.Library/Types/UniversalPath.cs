1af543b58f58ccf4ddf6717e9c6fe29df1c9739e
MakiseKurisu <saberconer@gmail.com>
2019-01-19 20:57:23 -0600

    Add UniversalPath

---
+++ Everything.NET.Library/Types/UniversalPath.cs
@@ -0,0 +1,39 @@
+﻿namespace Everything.NET.Library.Types
+{
+    public class UniversalPath
+    {
+        private string Path;
+        UniversalPath(string p)
+        {
+            Path = RemoveLeadingSlash(ConvertToUriPath(p));
+        }
+
+        public string RemoveLeadingSlash(string str)
+        {
+            while (str.IndexOf('/') == 0)
+            {
+                str = str.Substring(1);
+            }
+            return str;
+        }
+
+        public string UriPath => ConvertToUriPath(Path);
+        public string WindowsPath => ConvertToWindowsPath(Path);
+        public string WindowsSubfolderPath => ConvertToWindowsSubfolderPath(Path);
+
+        public string ConvertToUriPath(string str)
+        {
+            return str.Replace('\\', '/');
+        }
+
+        public string ConvertToWindowsPath(string str)
+        {
+            return str.Replace('/', '\\');
+        }
+
+        public string ConvertToWindowsSubfolderPath(string str)
+        {
+            return ConvertToWindowsPath(str).Replace(":", string.Empty);
+        }
+    }
+}
