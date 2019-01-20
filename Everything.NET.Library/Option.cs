3d9cddb461d6fb019d0feb958dbcd754dd9ae27f
MakiseKurisu <saberconer@gmail.com>
2019-01-19 21:49:38 -0600

    Implement Option for library

---
+++ Everything.NET.Library/Option.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Everything.NET.Library
+{
+    public static class Option
+    {
+        public static bool no_mime_type_check { get; set; } = false;
+    }
+}
